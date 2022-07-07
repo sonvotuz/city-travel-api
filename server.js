e38b65365b5c8c1fcb2fadb98630baefe0fd2a1b
Son Vo <vnsonvo@gmail.com>
2022-07-07 12:51:55 +0700

    add util to remove vietnamese characters for some cities names & code the api

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const cors = require("cors");
+const util = require("./util/remove-vietnamese-character");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 
-app.get("/", (req, res) => {});
+const cities = {
+  vietnam: {},
+  sapa: {},
+  hanoi: {},
+  quangninh: {},
+  hue: {},
+  danang: {},
+  nhatrang: {},
+  hochiminh: {},
+  phuquoc: {},
+};
 
-app.listen(PORT, () => {});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:city", (req, res) => {
+  let cityName = util.removeVietnameseCharacter(
+    req.params.city.toLowerCase().split(" ").join("")
+  );
+  if (cityName === "saigon") cityName = "hochiminh";
+  if (cities[cityName]) {
+    res.json(cities[cityName]);
+  } else {
+    res.json(cities["vietnam"]);
+  }
+});
+
+app.get("*", (req, res) => {
+  res.send("<h1>404</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is now running on port ${PORT}.`);
+});
